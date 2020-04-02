0bc84aa4c45f25f8fea792feb012e1ab5ce66bc7
algorythmist <dimitri@tecacet.com>
2020-04-02 08:39:03 -0700

    try using a redis service

---
+++ src/test/java/com/tecacet/redis/RedisConnectionTest.java
@@ -0,0 +1,17 @@
+package com.tecacet.redis;
+
+import org.junit.jupiter.api.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisConnectionTest {
+
+    @Test
+    public void connect() {
+        //Connecting to Redis server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server sucessfully");
+        //check whether server is running or not
+        System.out.println("Server is running: "+jedis.ping());
+    }
+}
